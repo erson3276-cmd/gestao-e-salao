ed505dbb022a2e453e6b260f7c5b7e60f1bba0ae
Dev Moca <dev@moca.com>
2026-04-07 11:35:12 -0300

    Corrige ID do Facebook Pixel

--- src/app/components/FacebookPixel.tsx
+++ src/app/components/FacebookPixel.tsx
@@ -19,7 +19,7 @@ export function FacebookPixel() {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2525455177911039');
+            fbq('init', '1309818977869718');
             fbq('track', 'PageView');
           `,
         }}
@@ -29,7 +29,7 @@ export function FacebookPixel() {
           height="1"
           width="1"
           style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=2525455177911039&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=1309818977869718&ev=PageView&noscript=1"
         />
       </noscript>
     </>
